6d9258230206cc933fc2c58b62b84704adae9df7
Hariom Chaturvedi <hariom4694@gmail.com>
2020-02-03 19:26:20 +0530

    Registeration is now functional

--- ionicproject/src/app/app.module.ts
+++ ionicproject/src/app/app.module.ts
@@ -9,17 +9,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
+    BrowserModule, 
+    IonicModule.forRoot(), 
     AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
-
-
   providers: [
     StatusBar,
     SplashScreen,
--- ionicproject/src/app/registration/registration.page.ts
+++ ionicproject/src/app/registration/registration.page.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+// import { TestBed } from '@angular/core/testing';
+
+interface User {
+  firstName: string,
+  lastName: string,
+  email?: string;
+  password?: string;
+}
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +16,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationPage implements OnInit {
 
-  constructor() { }
+  user: User = {
+    firstName: 'abc',
+    lastName: 'xyz',
+    email: 'test@test.com',
+    password: 'test1234',
+  };
+
+  constructor(public afAuth: AngularFireAuth) { }
 
   ngOnInit() {
+  }
+
+  async createAccount() {
+    const user = await this.afAuth.auth.createUserWithEmailAndPassword(
+      this.user.email,
+      this.user.password,
+    );
+
+    console.log(user);
   }
 
 }
